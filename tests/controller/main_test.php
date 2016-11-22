6c04d229d65957a79b36c3ac88a4140ddff08071
rinsrans <karl.rinser@gmail.com>
2016-11-23 00:29:22 +0100

    tests

--- tests/controller/main_test.php
+++ tests/controller/main_test.php
@@ -10,5 +10,16 @@ class main_test extends \phpbb_test_case
 		return array('rinsrans/guestbook');
 	}
 
+	public function setUp()
+	{
+		parent::setUp();
 
+	}
+
+	public function test_install()
+	{
+		$this->db = $this->new_dbal();
+		$db_tools = new \phpbb\db\tools($this->db);
+		$this->assertTrue($db_tools->sql_table_exists('phpbb_guestbook'));
+	}
 }
