0af9278581a5471b619c5fed0f87224df99b82a4
rinsrans <karl.rinser@gmail.com>
2016-11-23 01:30:28 +0100

    tests

--- tests/functional/phpbb_functional_test.php
+++ tests/functional/phpbb_functional_test.php
@@ -5,9 +5,16 @@
  */
 class phpbb_functional_test extends \phpbb_functional_test_case
 {
+	public function setUp()
+	{
+		parent::setUp();
+		$this->purge_cache();
+		$this->login();
+	}
+
     public function test_index()
     {
-        $crawler = $this->request('GET', 'index.php');
-        $this->assertGreaterThan(0, $crawler->filter('.topiclist')->count());
+        $crawler = $this->request('GET', 'app.php/guestbook');
+        $this->assertGreaterThan(0, $crawler->filter('.posts')->count());
     }
 }
